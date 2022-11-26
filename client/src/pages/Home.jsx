edc3405fc69659363b2b2bdaee872d3ed7863098
Shaz-hash <73599731+Shaz-hash@users.noreply.github.com>
2022-11-27 00:56:52 +0500

    edit
    few changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,8 +5,47 @@ import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
-  
-  
+  const [posts, setPosts] = useState([]);
+
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+  //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+  //     img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  // ];
 
   const getText = (html) =>{
     const doc = new DOMParser().parseFromString(html, "text/html")
@@ -16,85 +55,21 @@ const Home = () => {
 
   return (
     <div className="home">
-      
       <div className="posts">
-        <h4>Post 1</h4>
-        <h4>Author: Maaz Awaiz</h4>
-        <p>Welcome guys!!</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h2><br></br></h2>
-
-        <h4>Post 2</h4>
-        <h4>Author: Nameer </h4>
-        <p>I LOVE RIHANNA!!!!</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h4> </h4>
-
-        <h2><br></br></h2>
-
-        <h4>Post 3</h4>
-        <h4>Author: Mustafa Khokhar</h4>
-        <p>Mitochondria is the powerhouse of a cell</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h2><br></br></h2>
-
-
-
-        <h4>Post 4</h4>
-        <h4>Author: Azmeer</h4>
-        <p>OK</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h2><br></br></h2>
-
-
-        <h4>Post 5</h4>
-        <h4>Author: Shazer</h4>
-        <p>I am very happy today!!!!!!!</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h2><br></br></h2>
-
-
-        <h4>Post 6</h4>
-        <h4>Author: Maaz Awaiz</h4>
-        <p>I am very happy today!!!!!!!</p>
-        <div className ="btn">
-         <p> <button>LIKE</button>
-            <button>DISLIKE</button>
-            <button>Comment</button>
-           
-          </p>
-        </div>
-        <h2><br></br></h2>
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
+            </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc)}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
