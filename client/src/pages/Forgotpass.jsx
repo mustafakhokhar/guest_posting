286fab08b374bb4760a6ead99a887fc9aa628e80
Mustafa Amir Khokhar <mustafakhokhar26@gmail.com>
2022-12-01 17:39:19 +0500

    AdminPage

--- client/src/pages/Forgotpass.jsx
+++ client/src/pages/Forgotpass.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import { AuthContext } from "../context/authContext";
 
---
+++ client/src/pages/admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
+
+const Admin = () => {
+    
+};
+
+export default Admin;
