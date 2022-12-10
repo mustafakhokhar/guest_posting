affca7927a4c068c3ddfb9437f99868b3a586591
Mustafa Amir Khokhar <mustafakhokhar26@gmail.com>
2022-12-10 10:55:25 +0500

    wip

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import multer from "multer";
 import path from "path"
 
 const app = express();
-// app.use(express.static(path.join(__dirname+"/public")))
+app.use('/static',express.static((__dirname+"/")))
 app.use(express.json());
 app.use(cookieParser());
 const storage = multer.diskStorage({
@@ -32,7 +32,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/postsApproval", postsApprovalRoutes);
 
-const PORT = process.env.PORT || 8800
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log("Connected!");
